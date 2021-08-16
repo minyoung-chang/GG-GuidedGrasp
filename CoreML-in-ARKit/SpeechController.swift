dba08d2793d61fbc2901830595bab10dc10aa96f
Eric Jing <rune.dart4@gmail.com>
2021-08-15 23:15:58 -0400

    Added speech controller to storyboard

---
+++ CoreML-in-ARKit/SpeechController.swift
@@ -0,0 +1,12 @@
+//
+//  SpeechController.swift
+//  GuidedGrasp
+//
+//  Created by Eric Pu Jing on 8/15/21.
+//  Copyright © 2021 Yehor Chernenko. All rights reserved.
+//
+
+import Foundation
+
+class SpeechController: UIViewController {
+}
